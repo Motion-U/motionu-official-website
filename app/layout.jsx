452e2bb603c79a0311f7e5a024c8df38a50c1c73
hakimnazry24 <hakimnazry@gmail.com>
2024-10-02 11:42:02 +0000

    add : analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import FooterNew from "./components/footer-new";
@@ -33,17 +34,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} overflow-x-hidden relative max-w-[100vw]`}
-      >
-        <Alert />
-
-        <Header />
-        {children}
-        {/* <Footer /> */}
-        <FooterNew />
-      </body>
-    </html>
+    <Analytics>
+      <html lang="en">
+        <body
+          className={`${inter.className} overflow-x-hidden relative max-w-[100vw]`}
+        >
+          <Alert />
+          <Header />
+          {children}
+          {/* <Footer /> */}
+          <FooterNew />
+        </body>
+      </html>
+    </Analytics>
   );
 }
